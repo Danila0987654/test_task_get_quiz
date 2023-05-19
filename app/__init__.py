bcb1c535701779340f7fb3c7a8e6bbfea62d0448
DanilaSokolov <sokolovdanila2000@gmail.com>
2023-05-19 14:46:54 +0300

    Added libraries for a project
    Migrate and SQLAlchemy

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,17 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 from config import Config
 
+db = SQLAlchemy()
+migr = Migrate(db=db)
+
 app = Flask(__name__)
 app.config.from_object(Config)
+
+db.init_app(app)
+migr.init_app(app)
 
 from app.routes.main import bp as main_bp
 app.register_blueprint(main_bp)
